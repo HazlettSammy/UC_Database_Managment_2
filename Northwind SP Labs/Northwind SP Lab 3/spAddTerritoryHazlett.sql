5ac0849356a90b0c506bd201f14403772eae7377
Samuel Hazlett <119336439+HazlettSammy@users.noreply.github.com>
2025-03-11 14:40:31 -0400

    Add files via upload

---
+++ Northwind SP Labs/Northwind SP Lab 3/spAddTerritoryHazlett.sql
@@ -0,0 +1,34 @@
+--create the procedure 
+USE Northwind;
+GO
+
+CREATE PROCEDURE spAddTerritory
+    @TerritoryID NVARCHAR(20),
+    @TerritoryDescription NCHAR(50),
+    @RegionID INT
+AS
+BEGIN
+    BEGIN TRY
+        INSERT INTO Territories (TerritoryID, TerritoryDescription, RegionID)
+        VALUES (@TerritoryID, @TerritoryDescription, @RegionID);
+
+        PRINT 'Territory added successfully.';
+    END TRY
+    BEGIN CATCH
+        PRINT 'An error occurred while trying to insert the new territory.';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+GO
+
+-- Successful Insertion Test
+EXEC spAddTerritory 
+    @TerritoryID = '99999', 
+    @TerritoryDescription = 'New Test Territory', 
+    @RegionID = 1;
+
+	-- Error Test: Try to Duplicate TerritoryID (This will cause a primary key violation)
+EXEC spAddTerritory 
+    @TerritoryID = '99999', 
+    @TerritoryDescription = 'Duplicate Test Territory', 
+    @RegionID = 1;
